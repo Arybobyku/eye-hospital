4d8e8851e03a90d784ae2c80d0f2046812f23e76
teguh522 <teguhf522@gmail.com>
2024-03-13 11:57:32 +0700

    UPDATE FEATURE : url bridging di tambahkan

---
+++ resources/js/components/bpjs/bridging/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="inner" ref="roottable">
+        <div class="grid">
+            <div class="col-12">
+                <iframe title="bridging" width="100%" height="500" style="border: 0;"
+                    src="http://localhost:7000"></iframe>
+            </div>
+        </div>
+        <Loader ref="Loader"></Loader>
+    </div>
+</template>
