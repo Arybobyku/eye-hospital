8ac2639401fab191dca6b819cc4fc9e01c847d79
teguh522 <teguhf522@gmail.com>
2024-03-15 10:50:24 +0700

    UPDATE:url bridging di ganti

--- resources/js/components/bpjs/bridging/index.vue
+++ resources/js/components/bpjs/bridging/index.vue
@@ -3,7 +3,7 @@
         <div class="grid">
             <div class="col-12">
                 <iframe title="bridging" width="100%" height="500" style="border: 0;"
-                    src="http://localhost:7000"></iframe>
+                    src="http://192.168.100.20:7000"></iframe>
             </div>
         </div>
         <Loader ref="Loader"></Loader>
