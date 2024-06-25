3fd0447ce40766df3d748195e1bbb681caa7fcff
Ary Boby <ary.boby@sinarmasmining.com>
2024-06-26 00:57:38 +0700

    add template RM.1.3

--- app/Http/Controllers/RawatJalan/PrintRekamMedisCtrl.php
+++ app/Http/Controllers/RawatJalan/PrintRekamMedisCtrl.php
@@ -57,16 +57,12 @@ class PrintRekamMedisCtrl extends Controller
   function printRm1dot3($uuid)
   {
     $pdf = \App::make('dompdf.wrapper');
-		// $registrasi = Registrasi::where('uuid', '=', $uuid)->first();
-		// $pemeriksaanro = PemeriksaanRo::where('registrasi_uuid', '=', $uuid)->first();
-		// $pemeriksaandokter = PemeriksaanDokter::where('registrasi_uuid', '=', $uuid)->first();
-		// $pasien = Pasien::where('uuid', '=', $registrasi->pasien_uuid)->first();
     
     // $pdf->loadView('print.printrekammedis', compact('registrasi', 'pemeriksaanro', 'pemeriksaandokter', 'pasien'))->setPaper('a4', 'potrait');
-    // $pdf->loadView('print-rekam-medis.rawat-jalan.rm1dot3')->setPaper('a4', 'potrait');
+    $pdf->loadView('print-rekam-medis.rawat-jalan.rm1dot3')->setPaper('a4', 'potrait');
 
 		
-    // return $pdf->stream();
-	return view('print-rekam-medis.rawat-jalan.rm1dot3');
+    return $pdf->stream();
+	// return view('print-rekam-medis.rawat-jalan.rm1dot3');
   }
 }
--- resources/views/print-rekam-medis/rawat-jalan/rm1dot3.blade.php
+++ resources/views/print-rekam-medis/rawat-jalan/rm1dot3.blade.php
@@ -1,240 +1,226 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>RM1.1</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>RM1.3</title>
     <style>
-    @page { margin: 18px; }
-    body { margin: 18px; }
+        @page {
+            margin: 18px;
+        }
+
+        body {
+            margin: 18px;
+        }
+
         .wrap {
             width: 100%;
             height: auto;
             display: inline-block;
         }
-		
     </style>
-    
+
 </head>
+
 <body>
-<div style="position:fixed; right: 13px; bottom: 10px;">
-</div>
-<?php $fullpath = storage_path('app/public/images/header_rme.png');  ?>
-<div class="wrap">
-	<div style="width:100%; text-align:right; margin-bottom:5px">
-		RM 1.1/PU(GJ)/22
-	</div>
-    <div style="width: 100%;">
-        <table style="width: 100%; border-collapse: collapse;" border="1">
+    <div style="position:fixed; right: 13px; bottom: 10px;">
+    </div>
+    <?php $fullpath = storage_path('app/public/images/header_rme.png'); ?>
+    <div class="wrap">
+        <div style="width:100%; text-align:right; margin-bottom:5px">
+            RM 1.3/PKMRJ/22
+        </div>
+        <table style="border-collapse: collapse;">
+            {{-- HEADER --}}
             <tr style="border: 1px solid black;">
-                <td style="width: 60%">
-                    <img 
-                    style="width: 100%;"
-                    src="data:image/png;base64,
-                    <?php echo base64_encode(file_get_contents($fullpath)); ?>"
-                    />
-                </td>
-				<td style="width: 30%">
-					<table style="width: 100%" border="0">
-						<tr>
-							<td width="20%">Nama</td>
-							<td width="1%">:</td>
-							<td width=50%>..........</td>
-						</tr>
-						<tr>
-							<td width="20%">Tgl. Lahir</td>
-							<td width="1%">:</td>
-							<td width=50%>..........</td>
-						</tr>
-						<tr>
-							<td width="20%">No.RM</td>
-							<td width="1%">:</td>
-							<td width=50%>..........</td>
-						</tr>
-						<tr>
-							<td width="10%">NIK</td>
-							<td width="1%">:</td>
-							<td width=50%>..........</td>
-						</tr>
-					</table>
-                </td>
+                <div style="width: 100%;">
+                    <table style="width: 100%;">
+                        <tr style="border: 1px solid black;">
+                            <td style="border-right: 1px solid black; width:100%">
+                                <img style="width: 100%;"
+                                    src="data:image/png;base64,
+                            <?php echo base64_encode(file_get_contents($fullpath)); ?>" />
+                            </td>
+                            <td style="width: 50%">
+                                <table style="width: 100%" border="0">
+                                    <tr>
+                                        <td width="20%">Nama</td>
+                                        <td width="1%">:</td>
+                                        <td width=50%>..........</td>
+                                    </tr>
+                                    <tr>
+                                        <td width="20%">Tgl. Lahir</td>
+                                        <td width="1%">:</td>
+                                        <td width=50%>..........</td>
+                                    </tr>
+                                    <tr>
+                                        <td width="20%">No.RM</td>
+                                        <td width="1%">:</td>
+                                        <td width=50%>..........</td>
+                                    </tr>
+                                    <tr>
+                                        <td width="10%">NIK</td>
+                                        <td width="1%">:</td>
+                                        <td width=50%>..........</td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </tr>
+            {{-- PENGKAJIAN KEPERAWATAN MATA RAWAT JALAN --}}
+            <tr style="border: 1px solid black;">
+                <div style="width: 100%; text-align: center; margin-top: 0px; margin-top:5px; font-weight: bold">
+                    PENGKAJIAN KEPERAWATAN MATA RAWAT JALAN
+                </div>
+                <div style="width: 100%; text-align: center; margin-top: 0px; line-height: 21px; margin-bottom: 3px; ">
+                    <i>(dilengkapi dalam waktu 2 jam pertama pasien masuk ruang rawat jalan)</i>
+                </div>
+            </tr>
+            {{-- Tanggal --}}
+            <tr style="border: 1px solid black; width:100%">
+                <table style="border-collapse: collapse; width:100%">
+                    <tr>
+                        <td style="border-right: 1px solid black; width:100%">
+                            Tanggal:
+                        </td>
+                        <td style="border-right: 1px solid black; width:100%">
+                            Waktu:
+                        </td>
+                        <td style="width:100%">
+                            Perawat Pengkaji:
+                        </td>
+                    </tr>
+                </table>
+            </tr>
+            {{-- Status Fungsional --}}
+            <tr style="border: 1px solid black; width:100%">
+                <table style="border-collapse: collapse; width:100%">
+                    <tr>
+                        <td style="">
+                            Status Fungsional:
+                        </td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td><input type="checkbox" checked></td>
+                                    <td>Jalan tanpa bantuan</td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td><input type="checkbox" checked></td>
+                                    <td>Kursi Roda</td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td><input type="checkbox" checked></td>
+                                    <td>Tempat Tidur Dorong</td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td><input type="checkbox" checked></td>
+                                    <td>Jalan dengan bantuan</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </tr>
+            {{-- Keluhan Utama --}}
+            <tr style="border: 1px solid black; width:100%">
+                <table style="border-collapse: collapse; width:100%">
+                    <tr>
+                        <td style="border-right: 1px solid black; width:100%">
+                            <div style="width: 100%;height:50px"><b>Keluhan Utama</b></div>
+                            <div style="border-top: 1px solid black; width:100%; height:1px;"></div>
+                            <div style="width: 100%;height:100px"><b>Riwayat Penyakit:</b></div>
+                        </td>
+                        <td style="width:100%;">
+                            <b>KASUS URGENT:</b>
+                            <table>
+                                <tr>
+                                    <td><input type="checkbox" checked></td>
+                                    <td>MATA MERAH</td>
+                                </tr>
+                            </table>
+                            <table>
+                                <tr>
+                                    <td><input type="checkbox" checked></td>
+                                    <td>TRAUMA/KESAKITAN/</td>
+                                </tr>
+                            </table>
+                            <table>
+                                <tr>
+                                    <td><input type="checkbox" checked></td>
+                                    <td>MATA KABUR MENDADAK</td>
+                                </tr>
+                            </table>
+                            <table>
+                                <tr>
+                                    <td><input type="checkbox" checked></td>
+                                    <td>BALITA / MANULA</td>
+                                </tr>
+                            </table>
+                            <table>
+                                <tr>
+                                    <td><input type="checkbox" checked></td>
+                                    <td>LAINYA</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </tr>
+            {{-- PEMERIKSAAN FISIK --}}
+            <tr style="border: 1px solid black; width:100%;">
+                <b>PEMERIKSAAN FISIK</b>
+        </table>
+        </tr>
         </table>
     </div>
-	<br />
-
-	<div style="width: 100%; text-align: center; margin-top: 14px; margin-bottom: 3px; font-weight: bold">
-		PERSETUJUAN UMUM 
-	</div>
-
-
-	<div style="width: 100%; text-align: center; margin-top: 0px; line-height: 21px; margin-top: 20px; font-weight: bold">
-		(GENERAL CONSENT)
-	</div>
-
-	{{-- <div style="width: 100%; height: 1px; background: #353535;"></div>
-	<br /> --}}
-
-	<div style="width: 100%; text-align: center; margin-top: 0px; line-height: 21px; margin-bottom: 3px; margin-top:20px; font-weight: bold">
-		PASIEN/KELUARGA DAN ATAU WALI HUKUM HARUS MEMBACA, MEMAHAMI DAN MENGISI INFORMASI BERIKUT
-	</div>
-
-	<table style="width: 100%; margin-top: 20px" cellpadding="0" cellspacing="0">
-		<tr>
-			<td style="width: 100%"><b>1. PERSETUJUAN UNTUK PERAWATAN DAN PENGOBATAN</b></td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 10px; text-align: justify">&nbsp;&nbsp;&nbsp;&nbsp;Saya menyetujui untuk mendapatkan perawatan di Rumah Sakit Khusus Prima Vision Medan sebagai pasien rawat jalan/rawat inap 
-			tergantung kepada kebutuhan medis. Pengobatan dapat meliputi pemeriksaan visus, tonometri dan prosedur rutin seperti cairan infuse atau suntikan 
-		dan evaluasi (wawancara dan pemeriksaan fisik).</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 5px; text-align: justify">
-				&nbsp;&nbsp;&nbsp;&nbsp;Persetujuan yang saya berikan tidak termasuk persetujuan untuk prosedur/tindakan invasif (misalnya operasi) atau tindakan yang mempunyai resiko tingi.
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 5px; text-align: justify">
-				&nbsp;&nbsp;&nbsp;&nbsp;Jika saya memutuskan untuk menghentikan perawatan medis untuk diri saya, saya memahami dan menyadari bahwa Rumah Sakit Khusus Mata Prima Vision ataupun dokter tidak 
-			bertanggung jawab atau hasil yang merugikan saya.
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; padding-top: 15px">
-			<b>2. PERSETUJUAN PELEPASAN INFORMASI</b>
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 10px; text-align: justify">&nbsp;&nbsp;&nbsp;&nbsp;Saya memahami informasi yang ada di dalam Saya, termasuk diagnosis, hasil laboratorium dan hasil tes diagnostic yang digunakan untuk perawatan medis, 
-			Rumah Sakit Khusus Mata Prima Vision Medan akan menjamin kerahasiannya.
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 5px; text-align: justify">
-				&nbsp;&nbsp;&nbsp;&nbsp;Saya memberi wewenang kepada Rumah Sakit Khusus Mata Prima Vision Medan untuk memberikan informasi tentang diagnosis, hasil pelayanan dan pengobatan bila diperlukan untuk memproses 
-			klaim asuransi/perusahaan atau lembaga pemerintah.
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 5px; text-align: justify">
-				&nbsp;&nbsp;&nbsp;&nbsp;Saya memberi wewenang kepada Rumah Sakit Khusus Mata Prima Vision Medan untuk memberikan informasi tentang diagnosis, hasil pelayanan dan pengobatan saya kepada anggota keluarga saya 
-			dan kepada :
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; padding-top: 15px">
-			<b>3. HAK DAN TANGGUNG JAWAB PASIEN</b>
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 10px; text-align: justify">&nbsp;&nbsp;&nbsp;&nbsp;Saya memiliki hak untuk mengambil bagian dalam keputusan mengenai penyakit saya dalam hal perawatan medis dan rencana pengobatan. Saya telah 
-			mendapat informasi tentang Hak dan tanggung Jawab Pasien Rumah Sakit Khusus Mata Prima Vision Medan melalui leaflet dan banner yang disediakan petugas.
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 5px; text-align: justify">
-				&nbsp;&nbsp;&nbsp;&nbsp;Saya memahami bahwa Rumah Sakit Khusus Mata Prima Vision Medan tidak bertanggung jawab atas kehilangan barang-barang pribadi dan barang yang 
-			dibawa ke Rumah Sakit Khusus Mata Prima Vision Medan
-			</td>
-		</tr>
-
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 5px; text-align: justify">
-				&nbsp;&nbsp;&nbsp;&nbsp;Saya memahami bahwa Rumah Sakit Khusus Mata Prima Vision Medan tidak memperbolehkan pendokumentasian semua tindakan yang dilakukan di Rumah Sakit Khusus Mata Prima Vision, 
-			baik berbentuk Foto/Video/Audio (Sesuai UU Praktik Kedokteran No. 29/2004 Pasal 48 dan 51, UU Telekomunikasi No. 36/1999).
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; padding-top: 15px">
-			<b>4. INFORMASI RAWAT INAP</b>
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 10px; text-align: justify">&nbsp;&nbsp;&nbsp;&nbsp;Saya telah menerima informasi tentang peraturan yang telah diberlakukan oleh Rumah Sakit Khusus Mata Prima Vision Medan dan saya beserta keluarga 
-			bersedia untuk mematuhinya, termasuk mematuhi berkunjung pasien sesuai dengan aturan di Rumah Sakit Khusu Mata Prima Vision Medan.
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 5px; text-align: justify">
-				&nbsp;&nbsp;&nbsp;&nbsp;Anggota keluarga saya yang menunggu saya bersedia untuk selalu memakai tanda pengenal khusus yang diberikan Rumah Sakit Khusus Mata Prima Vision Medan, dan 
-			demi keamanan seluruh pasien setiap keluarga dan siapapun yang akan mengunjungi saya diluar jam berkunjung bersedia untuk diminta/diperiksa identitasnya dan 
-			memakai identitas yang diberikan Rumah Sakit Khusus Mata Prima Vision Medan.
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 5px; text-align: justify">
-				&nbsp;&nbsp;&nbsp;&nbsp;Saya memahami bahwa saya dapat memilih turun kelas perawatan apabila kamar perawatan yang menjadi hak saya sesuai fasilitas kartu BPJS saya tidak akan tersedia. Dan saya 
-			telah memahami apa yang menjadi kewajiban dan hak saya memilih hal tersebut. Dan saya bersedia mengikuti peraturan Rumah Sakit Khusus Mata Prima Vision Medan yang berlaku saat ini.
-			</td>
-		</tr>
-
-		<tr>
-			<td style="width: 100%; padding-top: 15px">
-			<b>5. BIAYA PERAWATAN</b>
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 10px; text-align: justify">&nbsp;&nbsp;&nbsp;&nbsp;Saya menyatakan setuju sebagai pasien/penanggung jawab dengan status umum untuk membayar total biaya perawatan yang diberikan sesuai rincian biaya dan ketentuan Rumah Sakit Khusus Mata Prima Vision Medan.
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 5px; text-align: justify">
-			Saya menyatakan setuju sebagai pasien/penanggung jawab pasien dengan biaya ditanggung penjamin untuk segera melengkapi berkas persyaratan administrasi paling lambat 3x24 jam.
-			</td>
-		</tr>
-		<tr>
-			<td style="width: 100%; font-size: 12pt; line-height: 22px; padding-top: 5px; text-align: justify">
-			Saya telah membaca isi dari pernyataan ini/telah dibacakan isi dari pernyataan ini, dan saya telah memahami isi dari pernyataan ini. Dan semua pernyataan saya telah dijawab dengan jelas.
-			</td>
-		</tr>
-	</table>
-
-	<table style="width: 100%; margin-top: 50px" cellpadding="0" cellspacing="0">
-		<tr>
-			<td style="width: 35%"><b>Medan, {{ date('d')}} {{ bulans(date('m')) }} {{ date('Y') }} {{ date('H') }}:{{ date('i') }} WIB</b></td>
-			<td rowspan="6" style="width: 35%"></td>
-			<td style="width: 30%"></td>
-		</tr>
-		<tr>
-			<td style="width: 35%; padding-top: 3px">Pemberi Informasi dari</td>
-			<td style="width: 30%; padding-top: 3px">Penerima Informasi</td>
-		</tr>
-		<tr>
-			<td style="width: 35%; padding-top: 3px">RS Khusus Mata Prima Vision</td>
-			<td style="width: 30%; padding-top: 3px">(Pasien/Keluarga Pasien)</td>
-		</tr>
-		<tr>
-			<td style="width: 35%; height: 90px"></td>
-			<td style="width: 30%; height: 90px"></td>
-		</tr>
-		<tr>
-			<td style="width: 35%"><span style="text-decoration: underline"><b>{{ \Crypt::decrypt(\Cookie::get(env('APP_IDENTIFIER').'Nama')) }}</b></span></td>
-			<td style="width: 30%"><span style="text-decoration: underline"><b></b></span></td>
-		</tr>
-		<tr>
-			<td style="width: 35%">Nama dan Tanda Tangan</td>
-			<td style="width: 30%">Nama dan Tanda Tangan</td>
-		</tr>
-	</table>
-
-</div>
 </body>
 <?php
 
-function bulans($bln) {
-	if ($bln == '01') { $bln = 'Januari'; }
-	else if ($bln == '02') { $bln = 'Februari'; }
-	else if ($bln == '03') { $bln = 'Maret'; }
-	else if ($bln == '04') { $bln = 'April'; }
-	else if ($bln == '05') { $bln = 'Mei'; }
-	else if ($bln == '06') { $bln = 'Juni'; }
-	else if ($bln == '07') { $bln = 'Juli'; }
-	else if ($bln == '08') { $bln = 'Agustus'; }
-	else if ($bln == '09') { $bln = 'September'; }
-	else if ($bln == '10') { $bln = 'Oktober'; }
-	else if ($bln == '11') { $bln = 'November'; }
-	else if ($bln == '12') { $bln = 'Desember'; }
-	return $bln;
+function bulans($bln)
+{
+    if ($bln == '01') {
+        $bln = 'Januari';
+    } elseif ($bln == '02') {
+        $bln = 'Februari';
+    } elseif ($bln == '03') {
+        $bln = 'Maret';
+    } elseif ($bln == '04') {
+        $bln = 'April';
+    } elseif ($bln == '05') {
+        $bln = 'Mei';
+    } elseif ($bln == '06') {
+        $bln = 'Juni';
+    } elseif ($bln == '07') {
+        $bln = 'Juli';
+    } elseif ($bln == '08') {
+        $bln = 'Agustus';
+    } elseif ($bln == '09') {
+        $bln = 'September';
+    } elseif ($bln == '10') {
+        $bln = 'Oktober';
+    } elseif ($bln == '11') {
+        $bln = 'November';
+    } elseif ($bln == '12') {
+        $bln = 'Desember';
+    }
+    return $bln;
 }
 
 ?>
+
 </html>
