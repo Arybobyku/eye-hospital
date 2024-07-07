88795c6865209dea20ea59873f3fbe482e9da090
Fauzan1512 <fauzanzaman1523@gmail.com>
2024-07-08 05:21:34 +0700

    Print8.7-9.0

--- app/Http/Controllers/RawatJalan/PrintRekamMedisCtrl.php
+++ app/Http/Controllers/RawatJalan/PrintRekamMedisCtrl.php
@@ -265,6 +265,40 @@ class PrintRekamMedisCtrl extends Controller
     return $pdf->stream();
     // return view('print-rekam-medis.rawat-jalan.rm1dot1',compact('pasien'));
   }
+  function printRm8dot7($uuid)
+  {
+    $pdf = \App::make('dompdf.wrapper');
+    // $registrasi = Registrasi::where('uuid', '=', $uuid)->first();
+    // $pemeriksaanro = PemeriksaanRo::where('registrasi_uuid', '=', $uuid)->first();
+    // $pemeriksaandokter = PemeriksaanDokter::where('registrasi_uuid', '=', $uuid)->first();
+
+    $pasien = Pasien::where('uuid', '=', $uuid)->first();
+    $pdf->loadView(
+      'print-rekam-medis.bedah.rm8dot7',
+      compact('pasien')
+    )->setPaper('a4', 'potrait');
+
+
+    return $pdf->stream();
+    // return view('print-rekam-medis.rawat-jalan.rm1dot1',compact('pasien'));
+  }
+  function printRm8dot9($uuid)
+  {
+    $pdf = \App::make('dompdf.wrapper');
+    // $registrasi = Registrasi::where('uuid', '=', $uuid)->first();
+    // $pemeriksaanro = PemeriksaanRo::where('registrasi_uuid', '=', $uuid)->first();
+    // $pemeriksaandokter = PemeriksaanDokter::where('registrasi_uuid', '=', $uuid)->first();
+
+    $pasien = Pasien::where('uuid', '=', $uuid)->first();
+    $pdf->loadView(
+      'print-rekam-medis.bedah.rm8dot9',
+      compact('pasien')
+    )->setPaper('a4', 'potrait');
+
+
+    return $pdf->stream();
+    // return view('print-rekam-medis.rawat-jalan.rm1dot1',compact('pasien'));
+  }
   function printRm4dot9($uuid)
   {
     $pdf = \App::make('dompdf.wrapper');
@@ -275,6 +309,40 @@ class PrintRekamMedisCtrl extends Controller
     $pasien = Pasien::where('uuid', '=', $uuid)->first();
     $pdf->loadView(
       'print-rekam-medis.bedah.rm4dot9',
+      compact('pasien')
+    )->setPaper('a4', 'potrait');
+
+
+    return $pdf->stream();
+    // return view('print-rekam-medis.rawat-jalan.rm1dot1',compact('pasien'));
+  }
+  function printRm8dot10($uuid)
+  {
+    $pdf = \App::make('dompdf.wrapper');
+    // $registrasi = Registrasi::where('uuid', '=', $uuid)->first();
+    // $pemeriksaanro = PemeriksaanRo::where('registrasi_uuid', '=', $uuid)->first();
+    // $pemeriksaandokter = PemeriksaanDokter::where('registrasi_uuid', '=', $uuid)->first();
+
+    $pasien = Pasien::where('uuid', '=', $uuid)->first();
+    $pdf->loadView(
+      'print-rekam-medis.bedah.rm8dot10',
+      compact('pasien')
+    )->setPaper('a4', 'potrait');
+
+
+    return $pdf->stream();
+    // return view('print-rekam-medis.rawat-jalan.rm1dot1',compact('pasien'));
+  }
+  function printRm9dot0($uuid)
+  {
+    $pdf = \App::make('dompdf.wrapper');
+    // $registrasi = Registrasi::where('uuid', '=', $uuid)->first();
+    // $pemeriksaanro = PemeriksaanRo::where('registrasi_uuid', '=', $uuid)->first();
+    // $pemeriksaandokter = PemeriksaanDokter::where('registrasi_uuid', '=', $uuid)->first();
+
+    $pasien = Pasien::where('uuid', '=', $uuid)->first();
+    $pdf->loadView(
+      'print-rekam-medis.bedah.rm9dot0',
       compact('pasien')
     )->setPaper('a4', 'potrait');
 
--- routes/print.php
+++ routes/print.php
@@ -42,10 +42,15 @@ Route::group(['middleware' => 'throttle: 250, 1'], function(){
 	Route::get('rekammedis/rawat-jalan/rm1dot7/{uuid}', [PrintRekamMedisCtrl::class, 'printRm1dot7']);
     Route::get('rekammedis/bedah/rm1dot10/{uuid}', [PrintRekamMedisCtrl::class, 'printRm1dot10']);
 	Route::get('rekammedis/bedah/rm1dot8/{uuid}', [PrintRekamMedisCtrl::class, 'printRm1dot8']);
+	Route::get('rekammedis/bedah/rm1dot9/{uuid}', [PrintRekamMedisCtrl::class, 'printRm1dot9']);
 	Route::get('rekammedis/bedah/rm2dot0/{uuid}', [PrintRekamMedisCtrl::class, 'printRm2dot0']);
 	Route::get('rekammedis/bedah/rm2dot2/{uuid}', [PrintRekamMedisCtrl::class, 'printRm2dot2']);
 	Route::get('rekammedis/bedah/rm2dot3/{uuid}', [PrintRekamMedisCtrl::class, 'printRm2dot3']);
 	Route::get('rekammedis/bedah/rm4dot9/{uuid}', [PrintRekamMedisCtrl::class, 'printRm4dot9']);
+	Route::get('rekammedis/bedah/rm8dot7/{uuid}', [PrintRekamMedisCtrl::class, 'printRm8dot7']);
+	Route::get('rekammedis/bedah/rm8dot9/{uuid}', [PrintRekamMedisCtrl::class, 'printRm8dot9']);
+	Route::get('rekammedis/bedah/rm8dot10/{uuid}', [PrintRekamMedisCtrl::class, 'printRm8dot10']);
+	Route::get('rekammedis/bedah/rm9dot0/{uuid}', [PrintRekamMedisCtrl::class, 'printRm9dot0']);
 	Route::get('rekammedis/rawat-jalan/all/{uuid}', [PrintRekamMedisCtrl::class, 'all']);
 
 });
