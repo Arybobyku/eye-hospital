8d694be109abc6fff1a156ec76e754bcbb0179e9
Ary Boby <ary.boby@sinarmasmining.com>
2024-08-06 18:39:31 +0700

    fix SOAP

--- resources/js/components/dokter/pemeriksaan/FormDetail.vue
+++ resources/js/components/dokter/pemeriksaan/FormDetail.vue
@@ -1658,75 +1658,160 @@
             doDigitalSignature: function() {
                 vm.form.ttd = window.localStorage.getItem("ttd") ?? "";
             },
-            setCkEditor: function() {
+            setCkEditor: function () {
+                console.log('===> SET CK EDITOR',vm.listobat[0]); 
                 vm.form.subject = vm.form.anamnese.value;
-                vm.form.assessment = vm.form.select.icd10.label;
+                vm.form.assessment = ``;
+                vm.form.plan = ``;
+
+
+                if (vm.listicdten.length != 0) { 
+                    vm.form.assessment = `
+                        <div>ICD 10</div>
+                        <ul>
+                        ${vm.listicdten.map((element)=> `<li>${element.nama_icdten}</li>` )}
+                        </ul>
+                    `;
+                }
+
+                if (vm.listicdnine.length != 0) {
+                    vm.form.plan += `
+                    <div>ICD 9</div>
+                        <ul>
+                        ${vm.listicdnine.map((element) => `<li>${element.nama_icdnine}</li>`)}
+                        </ul>
+                    <br>`;
+                }
+
+                if (vm.listobat.length != 0) {
+                    vm.form.plan += `
+                    <div>Obat</div>
+                    <figure class="table">
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td>Nama Obat</td>
+                                <td>Signa</td>
+                                <td>Posisi</td>
+                                <td>Qty</td>
+                            </tr>
+    
+                            ${vm.listobat.map((element) => `
+                                <tr>
+                                    <td>${element.nama}</td>
+                                    <td>${element.signa}</td>
+                                    <td>${element.posisimata}</td>
+                                    <td>${element.jumlah_kecil} ${element.nama_satuan_kecil}</td>
+                                </tr>
+                            `)}
+                            </tbody>
+                        </table>
+                        </figure>   
+                        `;
+                 }
+
+                if (vm.listobatracikan.length != 0) {
+                    vm.form.plan += `
+                    <div>Obat Racikan</div>
+                    <figure class="table">
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td>Nama Obat</td>
+                                <td>Signa</td>
+                                <td>Posisi</td>
+                                <td>Qty</td>
+                            </tr>
+    
+                            ${vm.listobatracikan.map((element) => `
+                                <tr>
+                                    <td>${element.nama}</td>
+                                    <td>${element.signa}</td>
+                                    <td>${element.posisimata}</td>
+                                    <td>${element.jumlah_kecil} ${element.nama_satuan_kecil}</td>
+                                </tr>
+                            `)}
+                            </tbody>
+                        </table>
+                        </figure>   
+                        `;
+                 }
+
+                if (vm.form.select.pilihanplan.value) {
+                    vm.form.plan += `
+                    <div>Planning</div>
+                        <ul>
+                           <li>${vm.form.select.pilihanplan.value}</li>
+                        </ul>
+                    `;
+                 }
+                 
                 vm.form.object = `
-            <figure class="table">
-            <table>
-                <tbody>
-                <tr>
-                    <td>&nbsp;</td>
-                    <td>Ocular Dextra</td>
-                    <td>Ocular Sinistra</td>
-                </tr>
-                <tr>
-                    <td>Autoref</td>
-                    <td>${vm.pemeriksaanro.ocular_dextra_autoref}</td>
-                    <td>${vm.pemeriksaanro.ocular_sinistra_autoref}</td>
-                </tr>
-                <tr>
-                    <td>Add</td>
-                    <td>${vm.pemeriksaanro.ocular_dextra_add}</td>
-                    <td>${vm.pemeriksaanro.ocular_sinistra_add}</td>
-                </tr>
-                <tr>
-                    <td>BCVA</td>
-                    <td>${vm.pemeriksaanro.ocular_dextra_bcva1} => ${vm.pemeriksaanro.ocular_dextra_bcva2}</td>
-                    <td>${vm.pemeriksaanro.ocular_sinistra_bcva1} => ${vm.pemeriksaanro.ocular_sinistra_bcva2}</td>
-                </tr>
-                <tr>
-                    <td>Keratometri K1</td>
-                    <td>${vm.pemeriksaanro.ocular_dextra_keratometri_k1}</td>
-                    <td>${vm.pemeriksaanro.ocular_sinistra_keratometri_k1}</td>
-                </tr>
-                <tr>
-                    <td>Keratometri K2</td>
-                    <td>${vm.pemeriksaanro.ocular_dextra_keratometri_k2}</td>
-                    <td>${vm.pemeriksaanro.ocular_sinistra_keratometri_k2}</td>
-                </tr>
-                <tr>
-                    <td>Tonometri</td>
-                    <td>${vm.pemeriksaanro.ocular_dextra_tonometri}</td>
-                    <td>${vm.pemeriksaanro.ocular_sinistra_tonometri}</td>
-                </tr>
-                <tr>
-                    <td>Visus</td>
-                    <td>${vm.pemeriksaanro.ocular_dextra_visus}</td>
-                    <td>${vm.pemeriksaanro.ocular_sinistra_visus}</td>
-                </tr>
-                <tr>
-                    <td>Kacamata Sph</td>
-                    <td>${vm.pemeriksaanro.ocular_dextra_kacamata_lama_sph}</td>
-                    <td>${vm.pemeriksaanro.ocular_sinistra_kacamata_lama_sph}</td>
-                </tr>
-                <tr>
-                    <td>Kacamata Cyl</td>
-                    <td>${vm.pemeriksaanro.ocular_dextra_kacamata_lama_cyl}</td>
-                    <td>${vm.pemeriksaanro.ocular_sinistra_kacamata_lama_cyl}</td>
-                </tr>
-                <tr>
-                    <td>Kacamata Add</td>
-                    <td>${vm.pemeriksaanro.ocular_dextra_kacamata_lama_addisi}</td>
-                    <td>${vm.pemeriksaanro.ocular_sinistra_kacamata_lama_addisi}</td>
-                </tr>
-                </tbody>
-            </table>
-            </figure>
+                    <figure class="table">
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>Ocular Dextra</td>
+                            <td>Ocular Sinistra</td>
+                        </tr>
+                        <tr>
+                            <td>Autoref</td>
+                            <td>${vm.pemeriksaanro.ocular_dextra_autoref}</td>
+                            <td>${vm.pemeriksaanro.ocular_sinistra_autoref}</td>
+                        </tr>
+                        <tr>
+                            <td>Add</td>
+                            <td>${vm.pemeriksaanro.ocular_dextra_add}</td>
+                            <td>${vm.pemeriksaanro.ocular_sinistra_add}</td>
+                        </tr>
+                        <tr>
+                            <td>BCVA</td>
+                            <td>${vm.pemeriksaanro.ocular_dextra_bcva1} => ${vm.pemeriksaanro.ocular_dextra_bcva2}</td>
+                            <td>${vm.pemeriksaanro.ocular_sinistra_bcva1} => ${vm.pemeriksaanro.ocular_sinistra_bcva2}</td>
+                        </tr>
+                        <tr>
+                            <td>Keratometri K1</td>
+                            <td>${vm.pemeriksaanro.ocular_dextra_keratometri_k1}</td>
+                            <td>${vm.pemeriksaanro.ocular_sinistra_keratometri_k1}</td>
+                        </tr>
+                        <tr>
+                            <td>Keratometri K2</td>
+                            <td>${vm.pemeriksaanro.ocular_dextra_keratometri_k2}</td>
+                            <td>${vm.pemeriksaanro.ocular_sinistra_keratometri_k2}</td>
+                        </tr>
+                        <tr>
+                            <td>Tonometri</td>
+                            <td>${vm.pemeriksaanro.ocular_dextra_tonometri}</td>
+                            <td>${vm.pemeriksaanro.ocular_sinistra_tonometri}</td>
+                        </tr>
+                        <tr>
+                            <td>Visus</td>
+                            <td>${vm.pemeriksaanro.ocular_dextra_visus}</td>
+                            <td>${vm.pemeriksaanro.ocular_sinistra_visus}</td>
+                        </tr>
+                        <tr>
+                            <td>Kacamata Sph</td>
+                            <td>${vm.pemeriksaanro.ocular_dextra_kacamata_lama_sph}</td>
+                            <td>${vm.pemeriksaanro.ocular_sinistra_kacamata_lama_sph}</td>
+                        </tr>
+                        <tr>
+                            <td>Kacamata Cyl</td>
+                            <td>${vm.pemeriksaanro.ocular_dextra_kacamata_lama_cyl}</td>
+                            <td>${vm.pemeriksaanro.ocular_sinistra_kacamata_lama_cyl}</td>
+                        </tr>
+                        <tr>
+                            <td>Kacamata Add</td>
+                            <td>${vm.pemeriksaanro.ocular_dextra_kacamata_lama_addisi}</td>
+                            <td>${vm.pemeriksaanro.ocular_sinistra_kacamata_lama_addisi}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    </figure>
             `;
 
 
-                console.log("====>", vm.form.object)
+                console.log("====>", vm.form.plan)
             },
 
             additemobat: function() {
@@ -2722,7 +2807,6 @@
             },
 
             dialog: function() {
-                console.log("KONTOL");
                 let text = "",
                     button = "";
                 if (vm.form.posisi == "adddata") {
