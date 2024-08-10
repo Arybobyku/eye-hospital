346600e3f0b6f6a679bb5716909258fff74de1ca
Ary Boby <ary.boby@sinarmasmining.com>
2024-08-10 17:00:27 +0700

    add order by cppt desc

--- app/Http/Controllers/RawatJalan/PrintRekamMedisCtrl.php
+++ app/Http/Controllers/RawatJalan/PrintRekamMedisCtrl.php
@@ -745,6 +745,7 @@ class PrintRekamMedisCtrl extends Controller
     $pasien = Pasien::where('uuid', '=', $uuid)->first();
     $cppt = Cppt::where('pasien_uuid', '=', $uuid)
             ->leftJoin('pengguna', 'cppt.pengguna_uuid', '=', 'pengguna.uuid')
+            ->orderBy('cppt.created_at', 'desc')
             ->get();    
 
      return view('print-rekam-medis.rawat-jalan.cppt',compact('pasien','cppt',));
