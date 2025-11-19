2041539ad4df12899bdcec9baa46fdded6ed073e
Ary Boby <ary.boby@sinarmasmining.com>
2025-11-19 10:52:02 +0700

    fix nullable print cashier

--- resources/views/print/printcashier.blade.php
+++ resources/views/print/printcashier.blade.php
@@ -150,7 +150,7 @@
                         $grandtotaltop = $grandtotaltop - $totalDiskonGlobalTop;
                         ?>
                     @endif
-                     @if ($registrasi->apakah_paket == 'Ya' && $paketBedah->harga_sudah_ditentukan == 1)
+                     @if ($registrasi->apakah_paket == 'Ya' && $paketBedah && $paketBedah->harga_sudah_ditentukan == 1)
                     Rp. {{ number_format($paketBedah->total) }}
                      @else
                     Rp. {{ number_format($grandtotaltop) }}
@@ -159,7 +159,7 @@
             </tr>
             <tr>
                 <td style="border: none; padding: 2px 5px">Terbilang</td>
-                 @if ($registrasi->apakah_paket == 'Ya' && $paketBedah->harga_sudah_ditentukan == 1)
+                 @if ($registrasi->apakah_paket == 'Ya' && $paketBedah && $paketBedah->harga_sudah_ditentukan == 1)
                 <td style="border: none; padding: 2px 5px">:
                     <i>"{{ terbilang($paketBedah->total) }} Rupiah"</i>
                 </td>
@@ -472,7 +472,7 @@
                     </tr>
                 @endif
                 {{-- Harga paket beda yang sudah ditentukan --}}
-                @if ($registrasi->apakah_paket == 'Ya' && $paketBedah->harga_sudah_ditentukan == 1)
+                @if ($registrasi->apakah_paket == 'Ya' && $paketBedah && $paketBedah->harga_sudah_ditentukan == 1)
                         <tr>
                             <td colspan="2" align="left" style="padding: 4px 7px; width: 65%;"><b>
                                 Diskon Paket
