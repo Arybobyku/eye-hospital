29343952ba993d4167a6bc017f7a505e2bbf2926
Ary Boby <ary.boby@sinarmasmining.com>
2026-04-27 20:54:51 +0700

    feat update config pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                // 'useTLS' => true,
                 //'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
 								// 'host' => '127.0.0.1',
                 //'port' => env('PUSHER_PORT', 443),
